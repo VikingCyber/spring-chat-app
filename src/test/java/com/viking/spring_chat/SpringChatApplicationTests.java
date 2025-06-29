f6ec5641f4ce0bbf1a004a53aa50f4cca7bafde8
VikingCyber <viking602ki557@mail.ru>
2025-06-29 17:08:23 +0700

    Add base project structure

---
+++ src/test/java/com/viking/spring_chat/SpringChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viking.spring_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
